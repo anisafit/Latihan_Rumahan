9e2f7363e930b232e4164d21e7026c28af2dc198
anisafit <118428759+anisafit@users.noreply.github.com>
2022-12-20 11:52:33 +0700

    ganti judul

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Latihan Rumahan',
+  tagline: 'make sports fun with us ',
+  url: 'https://latihan-rumahan.vercel.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
